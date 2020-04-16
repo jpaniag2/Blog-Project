9abb34046af1a681cad7b9e84ca57f13b81722b2
David Hotaran <dhotaran@uncc.edu>
2020-04-16 18:14:27 +0000

    Added password REGEX enforcement as one of our unique features

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,14 @@ class User < ApplicationRecord
     has_many :articles
     
     VALID_EMAIL_REGEX = /A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+    #password requires at least one capital letter, number and symbol
+    VALID_PASSOWORD_REGEX = /(?=[A-Z])(?=.+[0-9])(?=.+[!@#$^&*])/ 
     before_save { self.email = email.downcase }
     
     validates :name,
         presence: true,
-        length: { maximum: 12 }
+        length: { maximum: 12, minimum:6 }
+        
     
     validates :email,
         presence: true,
@@ -16,7 +19,8 @@ class User < ApplicationRecord
         
     validates :password,
         presence: true,
-        length: { minimum: 6 }
+        length: { minimum: 6 },
+        format: {with: VALID_PASSOWORD_REGEX}
         
     has_secure_password
 end
